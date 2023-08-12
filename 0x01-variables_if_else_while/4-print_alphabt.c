aa65281c673c0b317af0587972741526fa256917
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 16:18:09 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+*main - Entery point
+*Description: print values of n statues:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= z)
+	{
+	if (ch == 'e' || ch == 'q')
+		ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
