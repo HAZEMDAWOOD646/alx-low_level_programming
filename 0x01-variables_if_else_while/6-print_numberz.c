041022480f5b022761d6c16db2947715c9c33d8e
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 16:47:36 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+# include<stdio.h>
+/**
+*main - Entery point
+*Description: print values of n statues:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+	putcher(digit + '0');
+	digit++;
+	}
+	putchar("\n");
+
+	return (0);
+}
