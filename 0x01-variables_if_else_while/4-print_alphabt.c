e589a3ab3f0004c0e4f3d5f47dd5d1e7148db568
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 16:20:17 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= z)
+	while (ch <= 'z')
 	{
-	if (ch == 'e' || ch == 'q')
-		ch++;
+		if (ch == 'e' || ch == 'q')
+			ch++;
 		putchar(ch);
 		ch++;
 	}
