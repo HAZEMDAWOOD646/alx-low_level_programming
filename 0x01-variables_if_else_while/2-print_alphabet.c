08370cb0bed65630b157053d5f486f44313aeac7
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 15:47:22 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entery point
+*Description: print alphabet:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = "a";
+
+	while (ch <= "z")
+	{
+	putchar(ch);
+	ch++;
+	}
+	putchar("\n");
+	return (0);
+
+}
