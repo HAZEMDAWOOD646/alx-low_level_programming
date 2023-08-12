c9b66be9176a7f51bfcf99a41abefa701b8999cb
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 16:55:30 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+*main - Entery point
+*Description: print values of n statues:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	char vh = 'z';
+
+	while (vh <= 'a')
+	{
+	putchar(vh);
+	vh--;
+	}
+	putchar('\n');
+
+	return (0);
+}
