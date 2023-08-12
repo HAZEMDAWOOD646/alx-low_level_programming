8edfe3ec57fb8d5a8ca6ef75aa6df753b85e6f69
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 16:06:09 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - Entery point
+*Description: print values of n statues:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++
+	}
+
+	while (ch <= 'Z')
+	{
+	putchar(CH);
+	CH++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
