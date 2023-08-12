d179f63ce8a403470e4fe6ce962789c6bb70fe3c
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-12 16:27:30 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - Entery point
+*Description: print values of n statues:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+
+		print("%i", digit);
+		digit++;
+	}
+	print(\n);
+
+	return (0);
+}
