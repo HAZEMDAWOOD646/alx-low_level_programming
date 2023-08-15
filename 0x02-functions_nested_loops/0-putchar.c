ff4d51e3608e5bd199e4aa1388083c107c366d4b
HAZEMDAWOOD646 <hazemdawood646@gmail.com>
2023-08-15 11:13:47 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main - Entery point
+*Description: print values of n statues:
+*greater than, is zero and is not less than 6
+*Return: 0 (success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		-putchar(str[ch]);
+		_putchar('\n');
+
+		return (0);
+
+}
